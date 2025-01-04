083e65dd33d6f58e887181bd024c28d4d7676744
Oliver Forral <intrepion@gmail.com>
2025-01-04 14:26:36 -0700

    Added data rows for special characters and trimming.

--- Intrepion.HelloWorld/Intrepion.HelloWorld.BusinessLogic.UnitTests/ServicesTests/GreetingService_GreetShould.cs
+++ Intrepion.HelloWorld/Intrepion.HelloWorld.BusinessLogic.UnitTests/ServicesTests/GreetingService_GreetShould.cs
@@ -6,26 +6,36 @@ namespace Intrepion.HelloWorld.BusinessLogic.UnitTests.ServicesTests;
 public class GreetingService_GreetShould
 {
     [TestMethod]
-    public void Greet_NameAlice_ReturnHelloAlice()
+    [DataRow("Alice")]
+    [DataRow("     Alice       ")]
+    [DataRow(" \n Alice \n ")]
+    [DataRow(" \r   Alice \r  ")]
+    [DataRow("\t\t\tAlice\t\t\t")]
+    public void Greet_NameAlice_ReturnHelloAlice(string alice)
     {
         // Arrange
         var expected = "Hello, Alice!";
 
         // Act
-        var actual = GreetingService.Greet("Alice");
+        var actual = GreetingService.Greet(alice);
 
         // Assert
         Assert.AreEqual(expected, actual);
     }
 
     [TestMethod]
-    public void Greet_NameBob_ReturnHelloBob()
+    [DataRow("Bob")]
+    [DataRow("     Bob       ")]
+    [DataRow(" \n Bob \n ")]
+    [DataRow(" \r   Bob \r  ")]
+    [DataRow("\t\t\tBob\t\t\t")]
+    public void Greet_NameBob_ReturnHelloBob(string bob)
     {
         // Arrange
         var expected = "Hello, Bob!";
 
         // Act
-        var actual = GreetingService.Greet("Bob");
+        var actual = GreetingService.Greet(bob);
 
         // Assert
         Assert.AreEqual(expected, actual);
@@ -43,6 +53,23 @@ public class GreetingService_GreetShould
         // Arrange
         var expected = "Hello, world!";
 
+        // Act
+        var actual = GreetingService.Greet(name);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [DataRow("mary jane", "Hello, mary jane!")]
+    [DataRow("O'Connor", "Hello, O'Connor!")]
+    [DataRow("María", "Hello, María!")]
+    [DataRow("    多田野数人    ", "Hello, 多田野数人!")]
+    [DataRow("Smith-Jones", "Hello, Smith-Jones!")]
+    [DataRow("  First   Last  ", "Hello, First   Last!")]
+    [DataRow("😀", "Hello, 😀!")]
+    public void Greet_NameSpecial_ReturnSpecial(string name, string expected)
+    {
         // Act
         var actual = GreetingService.Greet(name);
 
