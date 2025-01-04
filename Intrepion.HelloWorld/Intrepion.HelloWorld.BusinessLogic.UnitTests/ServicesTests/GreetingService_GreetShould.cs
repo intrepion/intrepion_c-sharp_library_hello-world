2f662912e4c65008fc4f6015893164d5b8c76a42
Oliver Forral <intrepion@gmail.com>
2025-01-04 14:59:34 -0700

    First test with Alice.

---
+++ Intrepion.HelloWorld/Intrepion.HelloWorld.BusinessLogic.UnitTests/ServicesTests/GreetingService_GreetShould.cs
@@ -0,0 +1,19 @@
+using Intrepion.HelloWorld.BusinessLogic.Services;
+
+namespace Intrepion.HelloWorld.BusinessLogic.UnitTests.ServicesTests;
+
+public class GreetingService_GreetingShould
+{
+    [Test]
+    public async Task Greet_NameAlice_ReturnHelloAlice()
+    {
+        // Arrange
+        var expected = "Hello, Alice!";
+
+        // Act
+        var actual = GreetingService.Greet("Alice");
+
+        // Assert
+        await Assert.That(actual).IsEqualTo(expected);
+    }
+}
---
+++ Intrepion.HelloWorld/Intrepion.HelloWorld.BusinessLogic/Services/GreetingService.cs
@@ -0,0 +1,9 @@
+namespace Intrepion.HelloWorld.BusinessLogic.Services;
+
+public static class GreetingService
+{
+    public static string Greet(string? name)
+    {
+        return "Hello, Alice!";
+    }
+}
