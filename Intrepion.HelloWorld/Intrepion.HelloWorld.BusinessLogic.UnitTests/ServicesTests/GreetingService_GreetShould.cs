b4b6f39acd87dda515ba6dd6ea08fef1c05b89e7
Oliver Forral <intrepion@gmail.com>
2025-01-04 14:16:34 -0700

    Added second test for Bob.

--- Intrepion.HelloWorld/Intrepion.HelloWorld.BusinessLogic.UnitTests/ServicesTests/GreetingService_GreetShould.cs
+++ Intrepion.HelloWorld/Intrepion.HelloWorld.BusinessLogic.UnitTests/ServicesTests/GreetingService_GreetShould.cs
@@ -17,4 +17,17 @@ public class GreetingService_GreetShould
         // Assert
         Assert.AreEqual(expected, actual);
     }
+
+    [TestMethod]
+    public void Greet_NameBob_ReturnHelloBob()
+    {
+        // Arrange
+        var expected = "Hello, Bob!";
+
+        // Act
+        var actual = GreetingService.Greet("Bob");
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
 }
